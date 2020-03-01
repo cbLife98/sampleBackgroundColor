6cd8358db87b5862bbadb16b2964bd04545f1fd3
Isha <ishagar98@gmail.com>
2020-03-01 09:19:06 +0530

    Image

--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new MyHomeApp(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
